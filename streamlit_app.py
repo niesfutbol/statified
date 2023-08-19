59b35920a1af3ed1752f3bbbe9999a321e656d36
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-08-19 02:19:00 -0700

    ♻️ Use module `hierarchical_review_plots`

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from PIL import Image
 import plotly.express as px
 import streamlit as st
 import requests
+import hierarchical_review_plots as hrp
 import json
 
 
@@ -159,29 +160,8 @@ with team:
     played_minutes = data[data.team == team]
     wy_players = list_of_players_in_ws_and_as(larga, played_minutes)
     # Crear el gráfico de Altair
-    chart = (
-        alt.Chart(played_minutes, title=f"Minutes Played by Player and Match: \n{team}")
-        .mark_rect()
-        .encode(
-            alt.X("match:N", sort=alt.EncodingSortField(field="date", order="ascending")).title(
-                "Match"
-            ),
-            alt.Y(
-                "player:N",
-                sort=alt.EncodingSortField(field="minutes", op="sum", order="descending"),
-                title="Player",
-            ),
-            alt.Color("minutes:Q", scale=alt.Scale(scheme=color)).title("Minutes"),
-            tooltip=[
-                alt.Tooltip("match:N", title="Match"),
-                alt.Tooltip("player:N", title="Player"),
-                alt.Tooltip("minutes:Q", title="Minutes"),
-            ],
-        )
-        .configure_view(step=13, strokeWidth=0)
-        .configure_axis(domain=False, labelFontSize=10)
-    )
-    st.altair_chart(chart)
+    hm_consistent = hrp.make_heat_map_of_sonsistent(data, team, color)
+    st.altair_chart(hm_consistent)
 
     st.subheader("Repositories involved")
     """
